bb823b844ef0b028037d9aafa77b171fe2797dec
AsherSeiling <f14asher@gmail.com>
2020-06-19 17:28:03 -0500

    All butions added, front end finished.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // © 2020 Asher Seiling
+var user_input = 0;
+var computer_input = 0;
+
+
+// Run and lost messages
+var lost_message = "You lost!";
+var won_message = "You won!";
+
+// Main function
+function main(){ 
+};
+
+
+// Class to change the image
+class image{
+    static rock(){
+
+    };
+    static paper(){
+
+    };
+    static scissors(){
+
+    };
+};
